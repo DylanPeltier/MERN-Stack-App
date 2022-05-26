002351f64690df7ccaac9c4839790066284d117d
Dylan Peltier <dylandavidpeltier@gmail.com>
2022-05-25 23:19:51 -0400

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const colors = require('colors')
+
+connectDB()
 
 const app = express()
 
